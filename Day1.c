74e8be6e175f346dc3c18f73fc7d628fa44fa916
Anuvanshika Rana <122532406+Anuvanshika1@users.noreply.github.com>
2026-02-02 08:10:14 +0530

    Add files via upload

---
+++ Day1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int n, pos, x, i;
+    int arr[100];
+
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter the array elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to insert: ");
+    scanf("%d", &x);
+
+    printf("Enter the position (1-based): ");
+    scanf("%d", &pos);
+
+    // Shift elements to the right
+    for (i = n; i >= pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    // Insert the element
+    arr[pos - 1] = x;
+    n++; // increase size
+
+    printf("New array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
+
