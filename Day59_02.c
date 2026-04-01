90a1afba182a6cc2cc69d57c0799fdcc2cb18173
Anuvanshika Rana <122532406+Anuvanshika1@users.noreply.github.com>
2026-04-01 07:35:44 +0530

    Create Day59_02.c

---
+++ Day59_02.c
@@ -0,0 +1,61 @@
+/*Construct Binary Tree from Inorder and Postorder Traversal
+
+Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
+
+Example 1:
+Input: inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
+Output: [3,9,20,null,null,15,7]
+
+Example 2:
+Input: inorder = [-1], postorder = [-1]
+Output: [-1]
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+int findIndex(int* inorder, int start, int end, int val) {
+    for (int i = start; i <= end; i++) {
+        if (inorder[i] == val)
+            return i;
+    }
+    return -1;
+}
+
+struct TreeNode* build(int* inorder, int* postorder, int inStart, int inEnd, int* postIndex) {
+    if (inStart > inEnd)
+        return NULL;
+
+    int val = postorder[*postIndex];
+    (*postIndex)--;
+
+    struct TreeNode* root = newNode(val);
+
+    if (inStart == inEnd)
+        return root;
+
+    int index = findIndex(inorder, inStart, inEnd, val);
+
+    root->right = build(inorder, postorder, index + 1, inEnd, postIndex);
+    root->left  = build(inorder, postorder, inStart, index - 1, postIndex);
+
+    return root;
+}
+
+struct TreeNode* buildTree(int* inorder, int inorderSize, int* postorder, int postorderSize) {
+    int postIndex = postorderSize - 1;
+    return build(inorder, postorder, 0, inorderSize - 1, &postIndex);
+}
